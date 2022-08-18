c3256de289c076f0b14106beca3a2a986422258c
Fenrir-00 <96144320+Fenrir-00@users.noreply.github.com>
2022-08-18 11:55:59 +0200

    Update baner.py

--- baner.py
+++ baner.py
@@ -28,7 +28,7 @@ os.system("gem install lolcat")
 r= requests.get("https://raw.githubusercontent.com/Fenrir-00/baner-termux/main/version.txt")
 r=r.text
 print(r)
-if r != "version=1.2\n":
+if r != "version=1.3\n":
  os.system("clear")
  print(f"""{color.rojo}HAY UNA NUEVA VERSION ACTUALIZA EL REPOSITORIO
 HAY UNA NUEVA VERSION ACTUALIZA EL REPOSITORIO
@@ -67,7 +67,7 @@ def version():
  texto ="""
  |=======================================================|
  | Script by              : #FENRIR-00                   |
- | Version                : Version  1.2                 |
+ | Version                : Version  1.3                 |
  | Follow me on Github    : https://github.com/Fenrir-00 |
  | Contact me on Telegram : @Ritorito1990                |
  ========================================================= """
